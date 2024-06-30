7ba24f50a595c88d01ea8f3317b5633d9470ede3
Maria Eduarda <110746931+aeduardamariaa@users.noreply.github.com>
2024-06-30 20:18:41 -0300

    Create busca1.java

---
+++ lista3/busca1.java
@@ -0,0 +1,29 @@
+package lista3;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Busca1 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<Integer>();
+        Scanner sc = new Scanner(System.in);
+        int qtde = 0;
+        
+        for(int cont = 0; cont < 10; cont++ ) {
+            System.out.println("Informe o " + (cont+1) + "º número: ");
+            list.add(sc.nextInt());
+        }
+
+        System.out.println("Informe o valor que deseja procutar: ");
+        Integer busca = sc.nextInt();
+
+        for (Integer num : list) {
+            if (num.equals(busca))
+                qtde++;
+        }
+        
+        System.out.println("Número "+busca+ " encontrado "+ qtde + " vezes.");
+        
+    }
+}
