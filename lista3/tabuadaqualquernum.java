a40282623a7d0690f62c170cbc5552b6bec2f3cd
Maria Eduarda <110746931+aeduardamariaa@users.noreply.github.com>
2024-06-30 20:48:51 -0300

    Create tabuadaqualquernum.java

---
+++ lista3/tabuadaqualquernum.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe o número que deseja ver a tabuada: ");
+        Tabuada.tabuada(sc.nextInt());
+
+        sc.close();
+    }
+}
+
+class Tabuada {
+    public static void tabuada(int num) {
+        for(int i = 1; i <= 10; i++ ){
+            System.out.println(num + " x " + i + " = " + num * i);
+        }
+    }
+}
