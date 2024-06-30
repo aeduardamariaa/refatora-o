18fdb17c8397c2329ef3e3ab84340287d7cee130
Maria Eduarda <110746931+aeduardamariaa@users.noreply.github.com>
2024-06-30 20:02:31 -0300

    Update ex_01.java

--- lista3/ex_01.java
+++ lista3/ex_01.java
@@ -1,27 +1,55 @@
 package lista3;
 
+import java.util.Scanner;
+
 public class ex_01 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite seu código: ");
+        int cod = sc.nextInt();
+
+        System.out.println("Quantidade de horas normais: ");
+        float h_normais = sc.nextFloat();
+
+        System.out.println("Quantidade de horas extras: ");
+        float h_extra = sc.nextFloat();
+
+        sc.close();
+
+        Salario sal = new Salario(h_normais, h_extra);
+        float bruto = sal.sal_bruto();
+        float liquido = sal.sal_liquido();
+
+        ex_01.imprime(cod, h_normais, h_extra, bruto, liquido);
+    }
+
+    private static void imprime(int cod, float normal, float extra, float bruto, float liquido) {
+        System.out.println("O funcionário " + cod + " recebeu " + normal + " horas normais e " + extra + " horas extras.");
+        System.out.println("Irá receber " + bruto + " reais e terá " + liquido + " reais líquidos.");
+    }
 }
 
 class Salario {
 
-    public float sal_bruto(float h_normais, float h_extras) {
-        float total_normal = 0, total_extra = 0, bruto = 0;
+    private float horasn, horase;
 
-        total_normal = h_normais * 12f;
-        total_extra = h_extras * 15.50f;
-        bruto = total_extra + total_normal;
+    public Salario(float horasNormais, float horasExtras) {
+        this.horasn = horasNormais;
+        this.horase = horasExtras;
+    }
 
+    public float sal_bruto() {
+        float total_normal = this.horasn * 12f;
+        float total_extra = this.horase * 15.50f;
+        float bruto = total_extra + total_normal;
         return bruto;
     }
 
-    public float sal_liquido(float sal_bruto) {
-        float liquido = 0;
-
-        liquido = sal_bruto * 0.1f;
-        liquido = sal_bruto - liquido;
-
+    public float sal_liquido() {
+        float bruto = sal_bruto();
+        float liquido = bruto * 0.1f;
+        liquido = bruto - liquido;
         return liquido;
     }
 }
